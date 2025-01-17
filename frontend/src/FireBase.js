f32b7f7709aa427fbb9332a02d4878c4ef78252d
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2025-01-17 21:45:41 +0530

    updated

--- frontend/src/FireBase.js
+++ frontend/src/FireBase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage"; // Import storage module
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYgZ2r4MoK7fvyEanEsSqqQ2mnh5E-Z1M",
-  authDomain: "techtalk-co.firebaseapp.com",
-  projectId: "techtalk-co",
-  storageBucket: "techtalk-co.appspot.com", // Corrected storage bucket
-  messagingSenderId: "881905866671",
-  appId: "1:881905866671:web:3bab675d61b066187f815a",
-  measurementId: "G-GBBW62GZ98",
+  apiKey: Process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
